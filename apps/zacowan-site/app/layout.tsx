fb6427f9d7d93b1b7fb80f9fb7716188f7dc1305
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2023-06-22 17:22:07 -0500

    feat(site): basic homepage

--- apps/zacowan-site/app/layout.tsx
+++ apps/zacowan-site/app/layout.tsx
@@ -1,12 +1,19 @@
 import "./globals.css";
 
+import { JetBrains_Mono } from "next/font/google";
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={jetbrainsMono.className}>
       <body>{children}</body>
     </html>
   );
--- apps/zacowan-site/app/page.tsx
+++ apps/zacowan-site/app/page.tsx
@@ -1,11 +1,25 @@
 import { Button, Header } from "ui";
+import { Rubik } from "next/font/google";
+import { clsx } from "clsx";
+
+const rubik = Rubik({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Page() {
   return (
-    <>
-      <Header text="Web" />
-      <Button />
-      <p className="text-red-600">Test tailwind</p>
-    </>
+    <div className="bg-blue-600 left-0 right-0 top-0 bottom-0 absolute">
+      <div className="h-1/2">Image</div>
+      <div className="bg-white h-1/2 rounded-t-xl p-10 space-y-2">
+        <h1 className="uppercase font-bold text-3xl">Zach Cowan</h1>
+        <p className={clsx("text-gray-500 block", rubik.className)}>
+          Software engineer with an interest in design.
+        </p>
+        <div>
+          <h2 className="uppercase">Skills</h2>
+        </div>
+      </div>
+    </div>
   );
 }
