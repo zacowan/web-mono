ae960aaf016fe84b74cd6c430c104c38c73b33cd
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2023-09-10 10:04:39 -0400

    feat(nav): add breadcrumbs for words

---
+++ apps/personal-site/src/routes/words/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	$: paths = $page.route.id?.split('/').filter((path) => !!path) ?? [];
+</script>
+
+<div class="text-sm breadcrumbs mb-12">
+	<ul>
+		<li><a href="/">Home</a></li>
+		{#each paths as path}
+			<li><a class="capitalize" href={`/${path}`}>{path}</a></li>
+		{/each}
+	</ul>
+</div>
+<slot />
